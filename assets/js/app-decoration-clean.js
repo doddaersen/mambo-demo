d0f7bbc0b8332ab0e5da4c8d54f5ca2fc41d2b4c
doddaersen <vidvandre5@gmail.com>
2026-05-04 22:11:09 +0200

    Bump app demo cache after category order change

--- assets/js/app-decoration-clean.js
+++ assets/js/app-decoration-clean.js
@@ -1,6 +1,6 @@
 (()=>{
   const script=document.createElement('script');
-  script.src='assets/js/app-demo-clean.js?v=render-1';
+  script.src='assets/js/app-demo-clean.js?v=category-order-1';
   script.async=false;
   document.currentScript.after(script);
 })();
